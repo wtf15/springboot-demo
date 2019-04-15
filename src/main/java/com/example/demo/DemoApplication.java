b2d44b1e7c2bb222806fa33233a683145e9f3e48
wtf <2933670738@qq.com>
2019-04-15 15:46:16 +0800

    aop

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling  //定时任务
 public class DemoApplication {
 
 	public static void main(String[] args) {
